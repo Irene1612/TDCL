1a9ec0e6a92fcf4df547059ab7183744601e7fad
IreneGuerra <i.gferreras@alumnos.upm.es>
2014-10-29 13:55:03 +0100

    Fin de mañanaaaa!!!!

--- src/Practica2/javascript.js
+++ src/Practica2/javascript.js
@@ -1,32 +1,35 @@
 var numLineasDetalle = 1;
 var numDatos = 4;
-
-function fechaHoy() {
-	return new Date();
-}
+var fechaHoy = new Date();
 
 function onload() {
-	document.getElementById("fecha").innerHTML = "Fecha: " + fechaHoy().toLocaleDateString();
+	document.getElementById("fecha").innerHTML = "Fecha: " + fechaHoy.toLocaleDateString();
+	document.getElementById('calendarioFechaLimite').setAttribute("value", fechaHoy.getFullYear() + "-" + ("0" + (fechaHoy.getMonth() + 1)).slice(-2) + "-" +  ("0" + fechaHoy.getDate()).slice(-2));
 }
 
-function calcularFechaAbono() {//TODO ver q no meta una fecha anterior al dia de hoy
+function calcularFechaAbono() {
 	var numDias = 0;
-	var fecha;
-	if(document.getElementById('calendarioSi')==null){		
-		var radioButtons = document.getElementsByName('fechaLimite');		
-		for (var i = 0; i < radioButtons.length; i++) {
-			if (radioButtons[i].checked) {
-				numDias = radioButtons[i].value;
-			}
+	var fecha;		
+	var radioButtons = document.getElementsByName('fechaLimite');		
+	for (var i = 0; i < radioButtons.length-1; i++) {
+		if (radioButtons[i].checked) {
+			numDias = radioButtons[i].value;
 		}
-		fecha  = new Date(fechaHoy().getTime() + numDias*24*60*60*1000);
-	}else{
-		var ano = document.getElementById('calendarioFechaLimite').value.substring(0, 4);
-		var mes = document.getElementById('calendarioFechaLimite').value.substring(5, 7);
-		var dia = document.getElementById('calendarioFechaLimite').value.substring(8, 10);
-		fecha = new Date(ano, mes-1, dia);		
 	}
+	fecha  = new Date(fechaHoy.getTime() + numDias*24*60*60*1000);	
 	document.getElementById("fechaAbono").innerHTML = "Fecha límite de abono: " + fecha.toLocaleDateString();
+}
+
+function calcularFechaAbonoDesdeDiaConcreto(){
+	var ano = document.getElementById('calendarioFechaLimite').value.substring(0, 4);
+	var mes = document.getElementById('calendarioFechaLimite').value.substring(5, 7);
+	var dia = document.getElementById('calendarioFechaLimite').value.substring(8, 10);
+	var fecha = new Date(ano, mes-1, dia,  fechaHoy.getHours(), fechaHoy.getMinutes(), fechaHoy.getSeconds(), fechaHoy.getMilliseconds());
+	if(fechaHoy<=fecha){		
+		document.getElementById("fechaAbono").innerHTML = "Fecha límite de abono: " + fecha.toLocaleDateString();
+	}else{
+		alert("Ha introducido una fecha anterior al día de hoy.");
+	}
 }
 
 function aniadirLinea() {
@@ -72,11 +75,60 @@ function eliminarLinea() {
 }
 
 function mostrarCalendario() {
-	document.getElementById("fechaAbono").innerHTML = "Fecha límite de abono: -";
-	document.getElementById('calendarioNo').setAttribute("id","calendarioSi");
+	calcularFechaAbonoDesdeDiaConcreto();
+	if (document.getElementsByName('fechaLimite')[document.getElementsByName('fechaLimite').length-1].checked && document.getElementById('calendarioNo')!=null) {
+		document.getElementById('calendarioNo').setAttribute("id","calendarioSi");
+	}	
 }
 
 function ocultarCalendario() {
-	if(document.getElementById('calendarioSi')!=null)
+	if (!document.getElementsByName('fechaLimite')[document.getElementsByName('fechaLimite').length-1].checked && document.getElementById('calendarioSi')!=null) {
 		document.getElementById('calendarioSi').setAttribute("id","calendarioNo");
+	}
+}
+
+function ocultarCalendarioDesdeDiaConcreto() {
+	if (!document.getElementsByName('fechaLimite')[document.getElementsByName('fechaLimite').length-1].checked && document.getElementById('calendarioSi')!=null) {
+		document.getElementById('calendarioSi').setAttribute("id","calendarioNo");
+	}
+}
+
+function calcular() {
+	var vacio = false;
+	document.getElementById("subtotal").value = 0;	
+	document.getElementById("iva").value = 0;
+	document.getElementById("total").value = 0;	
+	for (var i = 0; i < numLineasDetalle; i++) {
+		document.getElementById("importe" + i).value = 0;
+		if(document.getElementById("precio" + i).value && document.getElementById("unidades" + i).value && document.getElementById("concepto" + i).value){
+			calcularImporte(i);
+			actualizarSubtotal(i);
+			actualizarIVA(i);
+			calcularTotal();
+		}else{
+			vacio =  true;			
+		}	
+	}	
+	if(vacio) alert("No están rellenos todos los campos de la factura.");
+}
+
+function calcularImporte(i) {
+	document.getElementById("importe" + i).value = parseFloat(document.getElementById("precio" + i).value) * parseInt(document.getElementById("unidades" + i).value);
+}
+
+function actualizarSubtotal(i) {	
+	document.getElementById("subtotal").value = (parseFloat(document.getElementById("subtotal").value) + parseFloat(document.getElementById("importe" + i).value)).toFixed(2);
+	
+}
+
+function actualizarIVA(i) {		
+	document.getElementById("iva").value = ((parseFloat(document.getElementById("iva").value) + parseFloat(document.getElementById("importe" + i).value))*21/100).toFixed(2);	
+}
+
+function calcularTotal(i) {	
+	document.getElementById("total").value = (parseFloat(document.getElementById("subtotal").value) + parseFloat(document.getElementById("iva").value)).toFixed(2);
+}
+
+function enviar() {
+	alert("Se han enviado los datos con éxito. (Vamos que falta por hacer esta función xD).");
 }
